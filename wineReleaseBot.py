80e7ac9b269871e52cad399232b946e7b4037080
RiderExMachina <riderexmachina@protonmail.com>
2019-07-08 22:21:21 -0700

    Updated to include Mastodon.
    Signed-off-by: RiderExMachina <riderexmachina@protonmail.com>

--- wineReleaseBot.py
+++ wineReleaseBot.py
@@ -1,23 +1,33 @@
 #!/usr/bin/env python3
 
 from bs4 import BeautifulSoup as bsoup
+from mastodon import Mastodon
 import requests, re, time, os, tweepy
 
 settingsConf = "settings.conf"
 stable = "0"
 devel = "0"
 
-with open("auth.cred", "r") as authFile:
-	authCred = authFile.readlines()
+with open("twitter-auth.cred", "r") as twitterAuth:
+	authCred = twitterAuth.readlines()
 	
-	CONSUMER_KEY = authCred[0].split(" ")[-1].strip("\n")
-	CONSUMER_SECRET = authCred[1].split(" ")[-1].strip("\n")
-	ACCESS_KEY = authCred[2].split(" ")[-1].strip("\n")
-	ACCESS_SECRET = authCred[3].split(" ")[-1].strip("\n")
+	TWITTER_CONSUMER_KEY = authCred[0].split(" ")[-1].strip("\n")
+	TWITTER_CONSUMER_SECRET = authCred[1].split(" ")[-1].strip("\n")
+	TWITTER_ACCESS_KEY = authCred[2].split(" ")[-1].strip("\n")
+	TWITTER_ACCESS_SECRET = authCred[3].split(" ")[-1].strip("\n")
 
-	auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-	auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-	api = tweepy.API(auth)
+with open("mast-auth.cred", "r") as mastAuth:
+	authCred = mastAuth.readlines()
+
+	MAST_CONSUMER_KEY = authCred[0].split(" ")[-1].strip("\n")
+	MAST_CONSUMER_SECRET = authCred[1].split(" ")[-1].strip("\n")
+	MAST_ACCESS_KEY = authCred[2].split(" ")[-1].strip("\n")
+
+auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
+auth.set_access_token(TWITTER_ACCESS_KEY, TWITTER_ACCESS_SECRET)
+twitter = tweepy.API(auth)
+
+mastodon = Mastodon(client_id=MAST_CONSUMER_KEY, client_secret=MAST_CONSUMER_SECRET, access_token=MAST_ACCESS_KEY, api_base_url='botsin.space')
 
 def versionCheck():
 	global stable, devel
@@ -34,7 +44,7 @@ def versionCheck():
 
 def main():
 	# Get information from winehq website
-	URL = "https://www.winehq.org/"
+	URL = "https://www.winehq.org"
 	page = requests.get(URL)
 	parsed = bsoup(page.content, 'html.parser')
 
@@ -54,13 +64,16 @@ def main():
 			with open(settingsConf + ".new", "w") as newSettings:
 				settings = settingsFile.readlines()
 				if stable != rawStable and devel == rawDevel:
-					api.update_status("WINE Stable has updated to version {}!\nCheck the release notes here: {}".format(rawStable, URL+versions[0]))
+					twitter.update_status("WINE Stable has updated to version {}!\nCheck the release notes here: {}".format(rawStable, URL+versions[0]))
+					mastodon.status_post("WINE Stable has updated to version {}!\nCheck the release notes here: {}".format(rawStable, URL+versions[0]))
 					newSettings.write("Stable: {}{}".format(rawStable, settings[1]))
 				elif stable == rawStable and devel != rawDevel:
-					api.update_status("WINE Development has updated to version {}!\nCheck the release notes here: {}".format(rawDevel, URL+versions[1]))
+					twitter.update_status("WINE Development has updated to version {}!\nCheck the release notes here: {}".format(rawDevel, URL+versions[1]))
+					mastodon.status_post("WINE Development has updated to version {}!\nCheck the release notes here: {}".format(rawDevel, URL+versions[1]))
 					newSettings.write("{}Development: {}".format(settings[0], rawDevel))
 				else:
-					api.update_status("WINE Stable has updated to version {} and WINE Development has updated to version {}!\nCheck the release notes here: \n{} (Stable)\n{} (Development)".format(rawStable, rawDevel, URL+versions[0], URL+versions[1]))
+					twitter.update_status("WINE Stable has updated to version {} and WINE Development has updated to version {}!\nCheck the release notes here: \n{} (Stable)\n{} (Development)".format(rawStable, rawDevel, URL+versions[0], URL+versions[1]))
+					mastodon.status_post("WINE Stable has updated to version {} and WINE Development has updated to version {}!\nCheck the release notes here: \n{} (Stable)\n{} (Development)".format(rawStable, rawDevel, URL+versions[0], URL+versions[1]))
 					newSettings.write("Stable: {}Development: {}".format(rawStable, rawDevel))
 		os.rename(settingsConf +".new", settingsConf)
 
