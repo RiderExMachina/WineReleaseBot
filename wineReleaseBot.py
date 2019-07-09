c7df9de549e82551ab71f6ca9daf7090e2c618d6
RiderExMachina <riderexmachina@protonmail.com>
2019-07-09 02:15:28 -0700

    Create catches in case __.cred does not exist. Add comments for easy website removal
    Signed-off-by: RiderExMachina <riderexmachina@protonmail.com>

--- wineReleaseBot.py
+++ wineReleaseBot.py
@@ -8,25 +8,41 @@ settingsConf = "settings.conf"
 stable = "0"
 devel = "0"
 
-with open("twitter-auth.cred", "r") as twitterAuth:
-	authCred = twitterAuth.readlines()
-	
-	TWITTER_CONSUMER_KEY = authCred[0].split(" ")[-1].strip("\n")
-	TWITTER_CONSUMER_SECRET = authCred[1].split(" ")[-1].strip("\n")
-	TWITTER_ACCESS_KEY = authCred[2].split(" ")[-1].strip("\n")
-	TWITTER_ACCESS_SECRET = authCred[3].split(" ")[-1].strip("\n")
+# Remove this if you don't want Twitter support
+if os.path.isfile("twitter-auth.cred"):
+	with open("twitter-auth.cred", "r") as twitterAuth:
+		authCred = twitterAuth.readlines()
+		
+		TWITTER_CONSUMER_KEY = authCred[0].split(" ")[-1].strip("\n")
+		TWITTER_CONSUMER_SECRET = authCred[1].split(" ")[-1].strip("\n")
+		TWITTER_ACCESS_KEY = authCred[2].split(" ")[-1].strip("\n")
+		TWITTER_ACCESS_SECRET = authCred[3].split(" ")[-1].strip("\n")
+else:
+	print("Please create a twitter-auth.cred file with your Twitter API information!")
+	exit()
+# End Twitter remove
 
-with open("mast-auth.cred", "r") as mastAuth:
-	authCred = mastAuth.readlines()
+# Remove this if you don't want Mastodon support
+if os.path.isfile("mast-auth.cred"):
+	with open("mast-auth.cred", "r") as mastAuth:
+		authCred = mastAuth.readlines()
 
-	MAST_CONSUMER_KEY = authCred[0].split(" ")[-1].strip("\n")
-	MAST_CONSUMER_SECRET = authCred[1].split(" ")[-1].strip("\n")
-	MAST_ACCESS_KEY = authCred[2].split(" ")[-1].strip("\n")
+		MAST_CONSUMER_KEY = authCred[0].split(" ")[-1].strip("\n")
+		MAST_CONSUMER_SECRET = authCred[1].split(" ")[-1].strip("\n")
+		MAST_ACCESS_KEY = authCred[2].split(" ")[-1].strip("\n")
+else:
+	print("Please create a mast-auth.cred file with your Mastodon API information!")
+	exit()
+# End Mastodon remove
 
+
+# Remove this if you don't want Twitter support
 auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
 auth.set_access_token(TWITTER_ACCESS_KEY, TWITTER_ACCESS_SECRET)
 twitter = tweepy.API(auth)
+# End Twitter remove
 
+#Remove the next line if you don't want mastodon support
 mastodon = Mastodon(client_id=MAST_CONSUMER_KEY, client_secret=MAST_CONSUMER_SECRET, access_token=MAST_ACCESS_KEY, api_base_url='botsin.space')
 
 def versionCheck():
@@ -65,16 +81,26 @@ def main():
 				settings = settingsFile.readlines()
 				if stable != rawStable and devel == rawDevel:
 					twitter.update_status("WINE Stable has updated to version {}!\nCheck the release notes here: {}".format(rawStable, URL+versions[0]))
+					print("Sent update to Twitter.")
 					mastodon.status_post("WINE Stable has updated to version {}!\nCheck the release notes here: {}".format(rawStable, URL+versions[0]))
+					print("Sent update to Mastodon.")
 					newSettings.write("Stable: {}{}".format(rawStable, settings[1]))
+					print("Written update to file.")
 				elif stable == rawStable and devel != rawDevel:
 					twitter.update_status("WINE Development has updated to version {}!\nCheck the release notes here: {}".format(rawDevel, URL+versions[1]))
+					print("Sent update to Twitter.")
 					mastodon.status_post("WINE Development has updated to version {}!\nCheck the release notes here: {}".format(rawDevel, URL+versions[1]))
+					print("Sent update to Mastodon.")
 					newSettings.write("{}Development: {}".format(settings[0], rawDevel))
+					print("Written update to file.")
 				else:
 					twitter.update_status("WINE Stable has updated to version {} and WINE Development has updated to version {}!\nCheck the release notes here: \n{} (Stable)\n{} (Development)".format(rawStable, rawDevel, URL+versions[0], URL+versions[1]))
+					print("Sent update to Twitter.")
 					mastodon.status_post("WINE Stable has updated to version {} and WINE Development has updated to version {}!\nCheck the release notes here: \n{} (Stable)\n{} (Development)".format(rawStable, rawDevel, URL+versions[0], URL+versions[1]))
+					print("Sent update to Mastodon.")
 					newSettings.write("Stable: {}Development: {}".format(rawStable, rawDevel))
+					print("Written update to file.")
+
 		os.rename(settingsConf +".new", settingsConf)
 
 	else:
@@ -83,4 +109,5 @@ def main():
 while True:
 	versionCheck()
 	main()
+	print("Job done! Sleeping for 3 hours.")
 	time.sleep(60**2 * 3.25)
