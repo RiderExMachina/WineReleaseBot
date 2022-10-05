b4b11373cf773ba10a1b57f523562161e7f3edb0
RiderExMachina <riderexmachina@protonmail.com>
2022-10-05 14:05:11 -0700

    Comments in main file, minor timer and installscript updates

--- install.sh
+++ install.sh
@@ -1,28 +1,33 @@
 #!/usr/bin/env bash
 
-if [[ $(id -u) -eq 0 ]]; then
-    echo "Please do not run this script as root!"
+if $1 -eq "uninstall"; then
+    sudo systemctl stop wrb.timer
+    sudo systemctl disable wrb.timer
+    rm /etc/systemd/system/wrb.{timer,service}
 fi
-if [[ ! type pip3 ]]; then
+
+if [[ $(id -u) -ne 0 ]]; then
+    echo "Please run this script as root"
+fi
+if [[ ! $(type pip3) ]]; then
     echo "Please install pip3"
     exit 1
 fi
 
 if [[ ! -d /opt/wrb ]]; then
-    mkdir -p /opt/wrb
+    sudo mkdir -p /opt/wrb
+fi
+sudo cp wineReleaseBot.py /opt/wrb/
+
+
+if [[ ! -d /etc/wrb ]]; then
+    sudo mkdir -p /etc/wrb
+fi
+if [[ ! -d $HOME/.config/wrb ]]; then
+    mkdir $HOME/.config/wrb
 fi
 
-cp wineReleaseBot.py /opt/wrb/
-cp systemd/wrb.service /etc/systemd/system/
-cp systemd/wrb.timer /etc/systemd/system/
-
 if [[ -f auth.cred ]]; then
-    if [[ ! -d /etc/wrb ]]; then
-        mkdir -p /etc/wrb
-    fi
-    if [[ ! -d $HOME/.config/wrb ]]; then
-        mkdir $HOME/.config/wrb
-    fi
     sudo cp auth.cred /etc/wrb/
     ln -s /etc/wrb $HOME/.config/wrb
 fi
@@ -30,9 +35,7 @@ fi
 /opt/wrb/wineReleaseBot.py -s
 /opt/wrb/wineReleaseBot.py -d
 
+sudo cp systemd/wrb.service /etc/systemd/system/
+sudo cp systemd/wrb.timer /etc/systemd/system/
 sudo systemctl enable wrb.timer
-sudo systemctl start wrb.service
-
-if [[!$ -ne 0]]; then
-	echo "Something went wrong. Please create a pull request with the information from 'systemctl status aacs-update'"
-fi
+sudo systemctl start wrb.timer
