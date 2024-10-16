20d9d9e458b33291bbc85e71e847ec29d05f3e68
RiderExMachina <riderexmachina@protonmail.com>
2024-10-15 21:51:35 -0700

    Fix how Wine info is pulled
    Removes bsoup dependency now that Wine is hosted using Gitlab and can use the API. Also removes tweepy as Twitter is no longer supported by this bot.

--- wineReleaseBot.py
+++ wineReleaseBot.py
@@ -44,12 +44,10 @@ if setup:
 	exit()
 ## Now that we know these should be installed, we can import them
 try:
-	from bs4 import BeautifulSoup as bsoup
 	from mastodon import Mastodon
-	import tweepy
-except:
+except ImportError as e:
 	if debug:
-		pass
+		relay(f"Importing modules: {e}")
 	else:
 		relay("\nOne of the dependencies has not been installed on this system!\nPlease run with the -s flag!\n")
 		quit()
@@ -57,8 +55,7 @@ except:
 ## Internal config and initializations
 settingsConf = os.path.join(settingsFolder, "settings.conf")
 authFile = os.path.join(settingsFolder, "auth.cred")
-stable = "0"
-devel = "0"
+wine = "0"
 proton = "0"
 dxvk = "0"
 ge = "0"
@@ -73,20 +70,6 @@ class style():
 	RESET = '\033[0m'
 ## Wizard to set up an auth.cred file if one is not found
 def newSetup(platform):
-	if platform == "Twitter":
-		relay("If you haven't already, please go to https://developer.twitter.com and get your App's keys and tokens.")
-		twit_con_key = input("Paste your API Key here > ").strip()
-		twit_con_sec = input("Paste your API Secret Key here > ").strip()
-		twit_acc_key = input("Paste your Access Token here > ").strip()
-		twit_acc_sec = input("Paste your Access Token Secret here >").strip()
-
-		relay(f"This information is being written to {authFile}. Please wait.")
-		with open(authFile, 'a') as twitterAuth:
-			twitInfo = {"twitter": [{"twit_api_key": twit_con_key, "twit_api_sec": twit_con_sec, "twit_access_key": twit_acc_key, "twit_access_secret": twit_acc_sec}]}
-			json.dump(twitInfo, twitterAuth, indent=4)
-		time.sleep(5)
-		relay(f"Data written to {authFile}. You are now ready to use the Twitter API!")
-
 	if platform == "Mastodon":
 		relay("If you haven't already, please go to your Mastodon's instance and get your keys and tokens.")
 		mast_con_key = input("Paste your API Key here > ").strip()
@@ -109,36 +92,6 @@ if debug:
 			relay("\t\tFake Mastodon post: {}".format(message))
 ## Import data from auth.cred
 if not debug:
-# Remove this if you don't want Twitter support
-	relay("\tLooking for Twitter information...")
-	if os.path.isfile(authFile):
-		with open(authFile, "r") as Auth:
-			twit_info = json.load(Auth)["twitter"]
-			relay("Found.")
-			for info in twit_info:
-				TWITTER_CONSUMER_KEY = info["twit_api_key"]
-				TWITTER_CONSUMER_SECRET = info["twit_api_secret"]
-				TWITTER_ACCESS_KEY = info["twit_access_key"]
-				TWITTER_ACCESS_SECRET = info["twit_access_secret"]
-		relay("\tTwitter information loaded successfully.")
-			
-	else:
-		relay(f"{style.RED}A file with your Twitter API information does not exist!{style.RESET}")
-		setup = input(f"Would you like to create one? [{style.GREEN}Y{style.RESET}/{style.RED}n{style.RESET}]")
-		affirmative = ["yes", "y", ""]
-		negative = ["no", "n"]
-		if setup.lower() in affirmative:
-			newSetup("Twitter")
-		if setup.lower() in negative:
-			relay(f"{style.YELLOW}Please comment out or remove the Twitter code.{style.RESET}")
-			exit()
-
-
-	auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
-	auth.set_access_token(TWITTER_ACCESS_KEY, TWITTER_ACCESS_SECRET)
-	twitter = tweepy.API(auth)
-	# End Twitter remove
-
 	# Remove this if you don't want Mastodon support
 	mastodonURL = "botsin.space"
 	relay("\t - Looking for Mastodon information...")
@@ -178,21 +131,19 @@ def clearOld():
 	if cleared == True: relay("\t- Old logs cleared out")
 ## Check cached versions from settings.conf
 def versionCheck():
-	global stable, devel, proton, dxvk, ge
+	global wine, proton, dxvk, ge
 	relay("\t- Checking cached versions of software...")
 	if os.path.isfile(settingsConf):
 		relay(f"\t- Opening {settingsConf}...")
 		with open(settingsConf, "r") as settingsFile:
 			settings = json.load(settingsFile)
-
-			stable = settings["Stable"]
-			devel = settings["Development"]
+			wine = settings["Wine"]
 			proton = settings["Proton"]
 			dxvk = settings["DXVK"]
 			ge = settings["GE"]
 
 		relay("\t\t--- From file ---")
-		relay(f"\t\tWine Stable:\t{stable}\n\t\tWine Devel:\t{devel}\n\t\tProton:\t{proton}\n\t\tDXVK:\t{dxvk}\n\t\tGE:\t{ge}\n")
+		relay(f"\t\tWine:\t{wine}\n\t\tProton:\t{proton}\n\t\tDXVK:\t{dxvk}\n\t\tGE:\t{ge}\n")
 
 def post(message):
 	relay(f"\t\t{style.CYAN}Posting update to Twitter.{style.RESET}")
@@ -204,17 +155,17 @@ def post(message):
 	relay(f"\t\t{style.GREEN}Updated to Mastodon successfully.\n{style.RESET}")
 ## Write updated information to settings.conf
 def write2File():
-	global stable, devel, proton, dxvk, ge
+	global wine, proton, dxvk, ge
 
 	relay("\tWriting to configuration file...")
 	with open(settingsConf + ".new", "w") as newSettings:
-		info = {"Stable":stable, "Development":devel, "Proton": proton, "DXVK": dxvk, "GE": ge}
+		info = {"Wine":wine, "Proton": proton, "DXVK": dxvk, "GE": ge}
 		json.dump(info, newSettings, indent=4)
 
 	os.rename(settingsConf + ".new", settingsConf)
 	relay("\tWritten to file.\n")
 ## Pull information from Github via Github API
-def getGithubInfo(project, url):
+def getGithubInfo(url):
 	page = requests.get(url)
 	information = page.json()
 
@@ -224,64 +175,50 @@ def getGithubInfo(project, url):
 
 	return directURL, release
 
+def getWineInfo(url):
+	page = requests.get(url)
+	information = page.json()
+
+	latest = information[0]
+	directURL = latest["_links"]["self"]
+	release = latest["tag_name"]
+
+	return directURL, release
+
 def main():
-	global stable, devel, proton, dxvk, ge
+	global wine, proton, dxvk, ge
 
 	# URLs of the different projects
 	URLs = {
-			"Wine":	  "https://www.winehq.org",
+			"Wine":	  "https://gitlab.winehq.org/api/v4/projects/5/releases",
 			"Proton": "https://api.github.com/repos/ValveSoftware/Proton/releases",
 			"DXVK":   "https://api.github.com/repos/doitsujin/dxvk/releases",
 			"GE": 	  "https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases"
 			}
 
 	for current in URLs:
-		## Start with Wine since it has a stable release cadence (avg every 2 weeks for Devel)
-		if current.lower() == "wine":
-			page = requests.get(URLs[current])
-			parsed = bsoup(page.content, 'html.parser')
-			# Look for anything with "/announce/" in the href. We can then just look at the first two in the list.
-			versions = [a['href'] for a in parsed.find_all(name="a", href=re.compile("/announce/"))]
+		if current == "Wine":
+			link, release = getWineInfo(URLs[current])
+		else:
+			link, release = getGithubInfo(URLs[current])
 
-			newStable = versions[0].split("/")[-1]
-			newDevel = versions[1].split("/")[-1]
+		if eval(current.lower()) != release:
+			relay(f"\t\t!!! {style.YELLOW}{current.upper()} UPDATE DETECTED!{style.RESET} !!!")
+			relay("\t\t--- From web -- \n\n\t\tLatest release: {}\n".format(release))
 
-			if stable != newStable or devel != newDevel:
-				relay(f"\t\t!!! {style.RED}WINE{style.RESET} {style.YELLOW}UPDATE DETECTED!{style.RESET} !!!")
-				relay("\t\t--- From web --- \n\n\t\tStable release: {} \n\t\tDevelopment release: {}\n".format(newStable, newDevel))
-				relay("\tWriting to configuration file.")
-				
-				if stable != newStable and devel == newDevel:
-					post("WINE Stable has updated to version {}!\nCheck the release notes here: {}".format(newStable, URLs[current]+versions[0]))
-					
-				elif stable == newStable and devel != newDevel:
-					post("WINE Development has updated to version {}!\nCheck the release notes here: {}".format(newDevel, URLs[current]+versions[1]))
+			post(f"{current} has updated to release {release}!\nCheck out the release here: {link}")
 
-				else:
-					post("WINE Stable has updated to version {} and WINE Development has updated to version {}!\nCheck the release notes here: \n{} (Stable)\n{} (Development)".format(newStable, newDevel, URLs[current]+versions[0], URLs[current]+versions[1]))
-				stable = newStable
-				devel = newDevel
-
-				write2File()
-
-
-		if current.lower() != "wine":
-			link, release = getGithubInfo(current, URLs[current])
-
-			if eval(current.lower()) != release:
-				relay(f"\t\t!!! {style.YELLOW}{current.upper()} UPDATE DETECTED!{style.RESET} !!!")
-				relay("\t\t--- From web -- \n\n\t\tLatest release: {}\n".format(release))
-				
-				post(f"{current} has updated to release {release}!\nCheck out the release here: {link}")
-				
-				if current == "Proton":
+			match current:
+				case "Wine":
+					wine = release
+				case "Proton":
 					proton = release
-				elif current == "DXVK":
+				case "DXVK":
 					dxvk = release
-				elif current == "GE":
+				case "GE":
 					ge = release
 
-				write2File()
+			write2File()
 	else:
 		relay("\tNo update detected.")
 if __name__ == "__main__":
